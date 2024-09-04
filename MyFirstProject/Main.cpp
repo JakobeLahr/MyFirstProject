37c7ee0c0c7b43d4d67fe105c20e0e8e7c567bed
Jakobe Lahr <lahr14@tritons.iowacentral.edu>
2024-09-03 22:03:07 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,18 @@
+/*
+	Jakobe Lahr
+	Computer Science FA24
+	Due: 9/3/24
+
+/*	
+	This is a simply program to print out hello world and my favorite color. 
+*/
+
+#include <iostream>		
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Red";
+
+	return 0;
+}
